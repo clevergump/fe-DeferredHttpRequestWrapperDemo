1eade30298f05af2dc7759c051d79658e6e07813
zhiyi.zhang <zhiyi.zhang@tcl.com>
2020-11-05 21:35:28 +0800

    add doc comment

--- js/demo.js
+++ js/demo.js
@@ -1,12 +1,22 @@
 ; (function () {
-    
+    /**
+     * defer执行http请求的工具类
+     * @type {{reqDeferredObj, deferReq}}
+     */
     var DeferredReqUtil = (function () {
         /**
+         * 发起多个deferred http请求, 只有当所有请求都得到成功响应后, 才会执行整体的成功回调函数 {@param allDeferredReqSuccessFn};
+         * 反之, 只要有一个请求执行失败, 就不再等待其他请求的执行, 直接提前执行整体的失败回调函数 {@param allDeferredReqFailFn}.
+         *
+         * 注意: defer整体的成功和失败回调函数都无参数, 而每个请求函数自己的回调函数是有参数的, 所以如果只有一个http请求函数,
+         * 即: {@param reqDeferredArray} 仅包含一个元素, 则建议不要使用defer整体的回调函数.
          *
          * @param shouldShowLoadingBar 加载时是否需要显示加载条
-         * @param reqDeferredArray           Type: Array()     An array of {@link reqDeferredObj} (deferred http requests).
+         * @param reqDeferredArray           Type: Array()     An array of {@link getDeferredReqObj} (deferred http requests).
          * @param allDeferredReqSuccessFn    Type: Function()  A function that is called when the Deferred is resolved.
+         *   注意: 该回调函数无参数
          * @param allDeferredReqFailFn       Type: Function()  A function that is called when the Deferred is rejected.
+         *   注意: 该回调函数无参数
          */
         function deferReq(shouldShowLoadingBar, reqDeferredArray, allDeferredReqSuccessFn,
             allDeferredReqFailFn) {
@@ -42,6 +52,14 @@
             }
         }
 
+        /**
+         * 代表一个http请求的 Deferred 对象
+         * @param reqFn       代表http请求的函数
+         * @param reqObj      代表http请求的请求参数
+         * @param onSuccessFn 参数是响应得到的对象
+         * @param onFailFn    参数是响应得到的对象
+         * @returns {*}
+         */
         function getDeferredReqObj(reqFn, reqObj, onSuccessFn, onFailFn) {
             if (!_.isFunction(reqFn)) {
                 throw new Error('reqFn must be typeof function');
@@ -136,8 +154,6 @@
             console.log(getCurrentDateTime()+"   " + appendixLog);
         }
     }
-
-    
 
     window.DeferredHttpRequestWrapper = {
         mockTest: mockTest,
